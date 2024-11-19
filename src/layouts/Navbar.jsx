d72805390ba89c66cb87fcab8f97a9fbfe83dbf9
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-11-19 10:08:41 -0300

    Add links to Navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import Logo from './../components/Logo';
+
+const links = [
+  { text: 'Home', to: '/' },
+  { text: 'About', to: '/about' },
+  { text: 'Sign in', to: '/sign-in' },
+  { text: 'Sign up', to: '/sign-up' },
+];
+
 export default function Navbar() {
   return (
-    <div className="bg-slate-100">
-      <div>Navbar</div>
+    <div className="flex justify-between gap-4 bg-slate-100 p-4">
+      <Logo />
+      <div className="hidden flex-none md:block">
+        <ul className="flex items-center gap-4">
+          {links.map((link) => (
+            <li key={link.text}>
+              <Link to={link.to}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="md:hidden">Hamburger Button</div>
     </div>
   );
 }
